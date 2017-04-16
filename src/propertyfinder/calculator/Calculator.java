2d39e7c2c3bd046b9edd3fba798c1a97cc2daa6e
khaled Roushdy <khaled_roushdy13@hotmail.com>
2017-04-16 02:08:19 +0200

    Add Calculator task

---
+++ src/propertyfinder/calculator/Calculator.java
@@ -0,0 +1,17 @@
+package propertyfinder.calculator;
+
+public class Calculator {
+
+	public double add(double x,double y){
+		return x+y;
+	}
+	
+	public double subtract(double x,double y){
+		return x-y;
+	}
+	
+	public double multiply(double x,double y){
+		return x*y;
+	}
+	
+}
---
+++ src/propertyfinder/calculator/tests/CalculatorTests.java
@@ -0,0 +1,48 @@
+package propertyfinder.calculator.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.Assert;
+import org.testng.AssertJUnit;
+import propertyfinder.calculator.Calculator;
+
+public class CalculatorTests {
+
+	private Calculator calculator ;
+	
+	@BeforeMethod
+	public void setup(){
+		
+		calculator = new Calculator();
+	}
+	
+	@Test
+	public void ShouldAddTwoPositiveNumber(){
+		
+		double expected = 7;
+		double actual=calculator.add(2, 5);
+		Assert.assertEquals(actual, expected, "The addition is not correct");
+	}
+	
+	@Test
+	public void ShouldSubtractTwoNumbers(){
+		
+		double expected = 1;
+		double actual=calculator.subtract(5, 4);
+		Assert.assertEquals(actual, expected, "The subtraction is not correct");
+	}
+	
+	@Test
+	public void ShouldMultiplyTwoNumbers(){
+		
+		double expected = 25;
+		double actual=calculator.multiply(5, 5);
+		Assert.assertEquals(actual, expected, "The multiplication is not correct");
+	}
+	
+	
+	
+	
+	
+	
+}
