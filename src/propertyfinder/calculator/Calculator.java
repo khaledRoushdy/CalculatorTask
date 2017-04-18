e07b7201b24f6e537356320f5b02f770249a1e6b
Khaled.Roushdy <Khaled.Roushdy@is-lt-7918.itworx.com>
2017-04-18 13:26:35 +0200

    Add new tests

--- src/propertyfinder/calculator/Calculator.java
+++ src/propertyfinder/calculator/Calculator.java
@@ -1,5 +1,7 @@
 package propertyfinder.calculator;
 
+import propertyfinder.calculator.Calculator.Invalid;
+
 public class Calculator {
 
 	public double add(double x,double y){
@@ -14,4 +16,20 @@ public class Calculator {
 		return x*y;
 	}
 	
+	public double divide (double x,double y) throws Invalid{
+		if(y == 0)
+			throw new Invalid("Can not divide by zero");
+		
+		else 
+			return x/y;
+	}
+	
+	
+	public class Invalid extends Exception {
+
+		  public Invalid(String message){
+		     super(message);
+		  }
+
+		}
 }
--- src/propertyfinder/calculator/tests/CalculatorTests.java
+++ src/propertyfinder/calculator/tests/CalculatorTests.java
@@ -5,44 +5,40 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.Assert;
 import org.testng.AssertJUnit;
 import propertyfinder.calculator.Calculator;
+import propertyfinder.calculator.Calculator.Invalid;
 
 public class CalculatorTests {
 
-	private Calculator calculator ;
-	
+	private Calculator calculator;
+
 	@BeforeMethod
-	public void setup(){
-		
+	public void setup() {
+
 		calculator = new Calculator();
 	}
-	
+
 	@Test
-	public void ShouldAddTwoPositiveNumber(){
-		
+	public void ShouldAddTwoPositiveNumber() {
+
 		double expected = 7;
-		double actual=calculator.add(2, 5);
+		double actual = calculator.add(2, 5);
 		Assert.assertEquals(actual, expected, "The addition is not correct");
 	}
-	
+
 	@Test
-	public void ShouldSubtractTwoNumbers(){
-		
+	public void ShouldSubtractTwoNumbers() {
+
 		double expected = 1;
-		double actual=calculator.subtract(5, 4);
+		double actual = calculator.subtract(5, 4);
 		Assert.assertEquals(actual, expected, "The subtraction is not correct");
 	}
-	
+
 	@Test
-	public void ShouldMultiplyTwoNumbers(){
-		
+	public void ShouldMultiplyTwoNumbers() {
+
 		double expected = 25;
-		double actual=calculator.multiply(5, 5);
+		double actual = calculator.multiply(5, 5);
 		Assert.assertEquals(actual, expected, "The multiplication is not correct");
 	}
-	
-	
-	
-	
-	
-	
+
 }
